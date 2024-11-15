284b1e14b871dab8abe1d22a15b4e2f26540eb25
PrimeBotz <primemovies4unayem@gmail.com>
2024-11-16 05:29:58 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-API_HASH = os.getenv("API_HASH", "27148454")
-API_ID = int(os.getenv("API_ID", "f668c20d77d1a8feee31afdc810f8ac4"))
+API_HASH = os.getenv("API_HASH", "f668c20d77d1a8feee31afdc810f8ac4")
+API_ID = int(os.getenv("API_ID", "27148454"))
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7509429916:AAFCto9RZEQlCmvd4rHCoz9Cvi863nI9_ZM")
 UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "Prime_Botz")
 BOT_OWNER = int(os.getenv("BOT_OWNER", "5926160191"))
