a1409727571f82d3ffbe9799b6383951dfccd8ac
PrimeBotz <primemovies4unayem@gmail.com>
2024-11-15 21:55:22 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-API_HASH = os.getenv("API_HASH", "")
-API_ID = int(os.getenv("API_ID", ""))
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+API_HASH = os.getenv("API_HASH", "27148454")
+API_ID = int(os.getenv("API_ID", "f668c20d77d1a8feee31afdc810f8ac4"))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7509429916:AAFCto9RZEQlCmvd4rHCoz9Cvi863nI9_ZM")
 UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "Prime_Botz")
-BOT_OWNER = int(os.getenv("BOT_OWNER", ""))
-DATABASE_URL = os.getenv("DATABASE_URL", "")
+BOT_OWNER = int(os.getenv("BOT_OWNER", "5926160191"))
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://cokas14279:yXIpH2JZnYfMHGHD@cluster0.dhfes.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
