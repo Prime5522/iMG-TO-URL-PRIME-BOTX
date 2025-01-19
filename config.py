ee80a1f7eaf736fc70dd542f8e19ed08decacaca
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-01-19 07:45:17 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002245813234').split()] 
+# give channel id with separate space. Ex: ('-10073828 -102782829 -1007282828')
+
 
 API_HASH = os.getenv("API_HASH", "f668c20d77d1a8feee31afdc810f8ac4")
 API_ID = int(os.getenv("API_ID", "27148454"))
